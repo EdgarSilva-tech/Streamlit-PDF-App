81c1f8b19ddc3474539f86173044573f3347aee0
EdgarSilva-tech <81367614+EdgarSilva-tech@users.noreply.github.com>
2023-10-20 06:51:30 +0100

    Initial streamlit app commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import os
+import tempfile
+from model import Retrieval_QA
+
+st.set_page_config(page_title='🦜🔗 Quick GPT')
+st.title("Quick GPT 📖📚🏫📝")
+st.sidebar.title("API Key")
+st.sidebar.subheader("Steps: ")
+st.sidebar.markdown("1 - Input your OpenAI API key")
+st.sidebar.markdown("2 - Insert a pdf file")
+st.sidebar.markdown("3 - Asks questions about your file")
+st.sidebar.markdown("4 - Have fun 😊")
+
+Open_AI_key = st.sidebar.text_input('Key:', type = 'password')
+
+pdf = st.file_uploader("Please insert your document here in PDF format: ", type="pdf")
+
+temp_file_path = os.getcwd()
+
+if pdf is not None:
+    # Save the uploaded file to a temporary location
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_file_path = os.path.join(temp_dir.name, pdf.name)
+    with open(temp_file_path, "wb") as temp_file:
+        temp_file.write(pdf.read())
+
+        st.write("Full path of the uploaded file:", temp_file_path)
+
+query_input = st.text_area(f"What do you want to know about {pdf.name}?", disabled=not Open_AI_key and not pdf)
+submitted = st.button('Submit')
+
+with st.spinner("Thinking about it..."):
+    if submitted and Open_AI_key.startswith('sk-'):
+        result = Retrieval_QA(temp_file_path, query_input, Open_AI_key)
+    elif submitted and not Open_AI_key.startswith("sk-"):
+        st.warning('Please enter a valid OpenAI API key!', icon='⚠')
---
+++ model.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.document_loaders import PyPDFLoader
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+def Retrieval_QA(pdf, query_input, key):
+
+    # Load and split the desired document
+    loader = PyPDFLoader(pdf)
+    
+    texts = loader.load_and_split()
+
+    # Define the model, embedding and vectorstore
+    llm = ChatOpenAI(streaming=True, temperature=0.0, max_tokens=1000, openai_api_key=key, callbacks=[StreamingStdOutCallbackHandler()])
+    embeddings = OpenAIEmbeddings(openai_api_key=key)
+
+    db = Chroma.from_documents(
+        texts,
+        embeddings
+    )
+
+    # Define retriever and memory variable to use in the model
+    retriever = db.as_retriever()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+    # Define the model and run the query
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        memory=memory,
+        verbose=False
+    )
+
+    answer = qa({"question": query_input}, return_only_outputs=True)
+
+    return st.write(answer)
