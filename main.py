8e23f700a0a274af1dc25f71fafa33bdce5d320a
EdgarSilva-tech <81367614+EdgarSilva-tech@users.noreply.github.com>
2023-10-25 04:42:48 +0100

    Document the code

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import os
 import tempfile
@@ -9,8 +6,10 @@ from model import Retrieval_QA
 from dotenv import load_dotenv
 load_dotenv()
 
+# Set Streamlit page configuration
 st.set_page_config(page_title='🦜🔗 Quick GPT')
 st.title("Quick GPT 📖📚🏫📝")
+
 st.sidebar.title("API Key")
 st.sidebar.subheader("Steps: ")
 st.sidebar.markdown("1 - Input your OpenAI API key")
@@ -18,11 +17,13 @@ st.sidebar.markdown("2 - Insert a pdf file")
 st.sidebar.markdown("3 - Asks questions about your file")
 st.sidebar.markdown("4 - Have fun 😊")
 
+# Get the OpenAI API key from the user
 Open_AI_key = st.sidebar.text_input('Key:', type = 'password', value=os.environ.get("OPENAI_API_KEY", None)
             or st.session_state.get("OPENAI_API_KEY", ""))
 
 st.session_state["OPENAI_API_KEY"] = Open_AI_key
 
+# Allow the user to upload a PDF document
 pdf = st.file_uploader("Please insert your document here in PDF format: ", type="pdf")
 
 temp_file_path = os.getcwd()
@@ -36,11 +37,14 @@ if pdf is not None:
 
         st.write("Full path of the uploaded file:", temp_file_path)
 
+# Get the user's query input
 query_input = st.text_area(f"What do you want to know about?", disabled=not Open_AI_key and not pdf)
 submitted = st.button('Submit')
 
+# Process the query with a spinner
 with st.spinner("Thinking about it..."):
     if submitted and Open_AI_key.startswith('sk-'):
+        # Call the Retrieval_QA function to get an answer from the PDF document
         result = Retrieval_QA(temp_file_path, query_input, Open_AI_key)
     elif submitted and not Open_AI_key.startswith("sk-"):
         st.warning('Please enter a valid OpenAI API key!', icon='⚠')
--- model.py
+++ model.py
@@ -10,6 +10,28 @@ from langchain.vectorstores.docarray.in_memory import DocArrayInMemorySearch
 
 def Retrieval_QA(pdf, query_input, key):
 
+    """
+    This function retrieves answers to questions from a PDF document using a conversational model.
+
+    Parameters:
+        pdf (str): The path to the PDF document.
+        query_input (str): The question or query for which you want to find an answer.
+        key (str): Your OpenAI API key for language model and embeddings.
+
+    Returns:
+        answer: The answer to the user's question is displayed using Streamlit.
+
+    Workflow:
+    1. Load and split the PDF document into text segments.
+    2. Initialize the language model (ChatOpenAI) and word embeddings (OpenAIEmbeddings) using the provided API key.
+    3. Create a Chroma vector store from the document texts.
+    4. Define a retriever to search for relevant information in the document.
+    5. Set up a memory buffer to maintain a conversation history.
+    6. Create a ConversationalRetrievalChain using the language model, retriever, and memory.
+    7. Query the model with the user-provided question.
+    8. Display the answer using Streamlit.
+    """
+
     # Load and split the desired document
     loader = PyPDFLoader(pdf)
     
